bce526dda81adfc98f41f810d6dae2fd8c69275b
Leo Wilkin <leo@wilkin.xyz>
2026-01-22 19:28:32 +0000

    Fix @/ path alias for Vite/Rollup in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import icon from "astro-icon";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://links.hackclub.com",
   integrations: [tailwind(), sitemap(), icon()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
 });
